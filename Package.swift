f7d2f5e9fb0681b15770943e492bf2f6dc3414f3
Joshua Park <jpark@duck.com>
2024-03-12 14:23:20 -0400

    Update swift-argument-parser to latest minor version
    This will also make ml-stable-diffusion more tolerant with regard to which swift-argument-parser version it needs, resulting in fewer version conflicts in Xcode projects.

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
             targets: ["StableDiffusionCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.2.0")
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.3")
     ],
     targets: [
         .target(
